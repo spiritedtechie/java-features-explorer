4848dae237f6911bf3226663a9add1c066c11e0c
Vijay Patel <spiritedtechie@gmail.com>
2024-04-17 12:22:51 +0100

    Playing with streams

---
+++ src/main/java/spiritedtechie/experiments/DuplicateStringsFilter.java
@@ -0,0 +1,40 @@
+package spiritedtechie.experiments;
+
+import java.util.stream.Collectors;
+import java.util.*;
+import java.util.stream.*;
+
+public class DuplicateStringsFilter {
+
+    public String deduplicate(List<String> names) {
+        return names.stream()
+                .distinct()
+                // .map((String s) -> {
+                // return s;
+                // })
+                .map(s -> s)
+                .collect(Collectors.joining(","));
+    }
+
+    public List<String> deduplicate_2(List<List<String>> names) {
+        return names.stream()
+                // .flatMap((List<String> a) -> {
+                // return a.stream();
+                // })
+                .flatMap(List::stream)
+                .distinct()
+                .collect(Collectors.toList());
+    }
+
+    public Map<String, Long> groupCount(List<List<String>> names) {
+         Map<String, Long> collect = names.stream()
+                // .flatMap((List<String> a) -> {
+                // return a.stream();
+                // })
+                .flatMap(List::stream)
+                .collect(Collectors.groupingBy(String::toString, Collectors.counting()));
+
+        return collect;
+    }
+
+}
---
+++ src/test/java/spiritedtechie/experiments/DuplicateStringsFilterTest.java
@@ -0,0 +1,45 @@
+package spiritedtechie.experiments;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+
+public class DuplicateStringsFilterTest {
+
+    private final DuplicateStringsFilter filter = new DuplicateStringsFilter();
+
+    @Test
+    public void testDeduplication() {
+
+        List<String> names = List.of("bob", "john", "bob", "fred", "fred", "george");
+
+        String result = filter.deduplicate(names);
+
+        assertEquals("bob,john,fred,george", result);
+    };
+
+    @Test
+    public void testDeduplicationListOfLists() {
+
+        List<List<String>> names = List.of(List.of("bob", "john"), List.of("bob", "fred", "fred"), List.of("george"));
+
+        List<String> result = filter.deduplicate_2(names);
+
+        assertEquals(List.of("bob", "john", "fred", "george"), result);
+    }
+
+    @Test
+    public void testGroupCount() {
+
+        List<List<String>> names = List.of(List.of("bob", "john"), List.of("bob", "fred", "fred"), List.of("george"));
+
+        Map<String, Long> result = filter.groupCount(names);
+
+        Map<String, Long> expected = Map.of("bob", 2L, "john", 1L, "fred", 2L, "george", 1L);
+
+        assertEquals(expected, result);
+    }
+}
